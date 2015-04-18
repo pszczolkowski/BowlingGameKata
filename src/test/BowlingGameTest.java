328a93ad8b41288919636c456a4b9b5d63605122
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-18 09:48:30 +0200

    Test 2 - refactorization
    * added set up method which initialize game before running tests
    * refactorized rollMany arguments

--- src/test/BowlingGameTest.java
+++ src/test/BowlingGameTest.java
@@ -4,27 +4,33 @@ import static org.junit.Assert.assertThat;
 import static test.GameScoreMather.finishedWithScore;
 import main.Game;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
+	private Game game;
+	
+	@Before
+	public void setUp(){
+		game = new Game();
+	}
+	
 	@Test
 	public void gameWithNoKnockDowns_expectedScore0() {
-		Game game = new Game();		
-		rollMany(game, 0, 20);
+		rollMany(20 , 0);
 		
 		assertThat( game , finishedWithScore( 0 ) );
 	}
 	
 	@Test
 	public void gameWithAllOnes_expectedScore20(){
-		Game game = new Game();
-		rollMany(game, 1, 20);
+		rollMany(20 , 1);
 		
 		assertThat(game, finishedWithScore( 20 ));
 	}
 
-	private void rollMany(Game game, int pins, int n) {
+	private void rollMany( int n , int pins ) {
 		for( int i = 0; i < n ; i++ ) {
 			game.roll( pins );
 		}

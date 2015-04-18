75e3413242fc93ce3ab252814affeacd748fde1e
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-04-18 10:02:43 +0200

    Test 3 - success
    Modified score counting and game rolling logic

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,13 +2,28 @@ package main;
 
 public class Game {
 	
-	private int score = 0;
+	private int[] rolls = new int[ 21 ];
+	private int currentRoll = 0;
 
 	public void roll( int pins ) {
-		score += pins;
+		rolls[ currentRoll ] = pins;
+		currentRoll += 1;
 	}
 
 	public int score() {
+		int score = 0;
+		int frameIndex = 0;
+		
+		for( int frame = 0 ; frame < 10 ; frame++ ){
+			if( rolls[ frameIndex ] + rolls[ frameIndex + 1 ] == 10 ){ //spare
+				score += 10 + rolls[ frameIndex + 2 ];
+				frameIndex += 2;
+			}else{
+				score += rolls[ frameIndex ] + rolls[ frameIndex + 1 ];
+				frameIndex += 2;
+			}
+		}
+		
 		return score;
 	}
 

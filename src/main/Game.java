4c7cb7b36c30d6f60617f038caa78833808e4d1d
Krzysztof Pszczółkowski <pszczolkowski.k@gmail.com>
2015-05-14 10:23:42 +0200

    test 3 - green

--- src/main/Game.java
+++ src/main/Game.java
@@ -2,13 +2,29 @@ package main;
 
 public class Game {
 	
-	private int score;
+	private int[] rolls = new int[ 21 ];
+	private int currentRoll = 0;
 
 	public void roll(int pins) {
-		score += pins;
+		rolls[ currentRoll ] = pins;
+		
+		currentRoll += 1;
 	}
 
 	public int score() {
+		int score = 0;
+		int i = 0;
+		
+		for( int frame = 0 ; frame < 10 ; frame++ ){
+			if( rolls[ i ] + rolls[ i+1 ] == 10 ){	//SPARE
+				score += 10 + rolls[ i + 2 ];
+				i += 2;
+			}else{
+				score += rolls[ i ] + rolls[ i+1 ];
+				i += 2;
+			}
+		}
+		
 		return score;
 	}
 
